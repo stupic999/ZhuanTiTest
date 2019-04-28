87d3344c5f9deab1b69f1130d7ef555c7f21ac1f
stupid999 <aierzhiguang2@gmail.com>
2019-04-28 15:37:13 +0800

    加入新物件： Start Scene StartButton SettingButton CrefitButton ExitButton
    加入新功能：
    點擊StartBtn會切換Scene到SampleScene
    點擊SettingBtn會出現SettingUI(未完成)，並把BtnUI隱藏
    點擊CreditBtn會出現CrefditUI(未完成)，並把BtnUI隱藏
    點擊CloseBtn（也就是x，會出現在SettingUI和CreditUI）會把SettingUI和CredtiUI隱藏，並把BtnUI顯示
    點擊ExitBtn，會退出遊戲

--- Assets/ButtonEffect.cs
+++ Assets/ButtonEffect.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonEffect : MonoBehaviour {
 
+    public GameObject SettingUI;
+    public GameObject CreditUI;
+    public GameObject BtnUI;
+
+    private void Start()
+    {
+        ShowBtnUI();
+    }
+
     public void StartBtn()
     {
-        Debug.Log("Start");
+        SceneManager.LoadScene("SampleScene");
     }
     public void SettingBtn()
     {
-        Debug.Log("Setting");
+        BtnUI.SetActive(false);
+        SettingUI.SetActive(true);
     }
     public void CreditBtn()
     {
-        Debug.Log("Credit");
+        BtnUI.SetActive(false);
+        CreditUI.SetActive(true);
     }
     public void ExitBtn()
     {
-        Debug.Log("Exit");
+        Application.Quit();
+    }
+    public void ShowBtnUI()
+    {
+        SettingUI.SetActive(false);
+        CreditUI.SetActive(false);
+        BtnUI.SetActive(true);
     }
 }
